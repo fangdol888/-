d3843487707713e5fa1cba82cb8bf39d36648e86
fangdol888 <fangdol888@gmail.com>
2021-04-14 12:32:45 +0900

    basic function and component is created

--- javascript/timer.js
+++ javascript/timer.js
@@ -1,5 +1,7 @@
 var min;
 var rest;
+var stopping;
+var start = false;
 function timer(){
     min = document.getElementById("minute").value;
     rest = document.getElementById("rest").value;
@@ -9,8 +11,12 @@ function timer(){
     else if(min == "" || rest ==""){
         alert("빈칸이 있습니다. 숫자를 입력하세요");
     }else{
-        var started = new Date();
-        showTime(started);
+        if(!start){
+            var started = new Date();
+            stopping= false;
+            start=true;
+            showTime(started);
+        }
     }
 }
 
@@ -20,9 +26,17 @@ function showTime(started){
     var diff = min * 60 * 1000 - (now.getTime() - started.getTime());
     var second = Math.floor(diff /1000) % 60;
     var minute = Math.floor(diff/1000/60) % 60;
-    text.innerHTML= "다음 쉬는 시간 까지... " + minute + "분 " + second+" 초";
+
+    text.innerHTML= "다음 쉬는 시간 까지... <br>" + minute + "분 " + second+" 초";
+
+    if(stopping){
+        stopping = false;
+        text.innerHTML = "중지되었습니다.";
+        return;
+    }
     if(diff > 0){
         setTimeout(showTime, 100, started);
+        
     }
     else{
         resting(now);
@@ -35,11 +49,21 @@ function resting(started){
     var diff = min * 60 * 1000 - (now.getTime() - started.getTime());
     var second = Math.floor(diff /1000) % 60;
     var minute = Math.floor(diff/1000/60) % 60;
-    text.innerHTML= "남은 쉬는시간은... " + minute + "분 " + second+" 초";
-    if(diff > 0){
+    text.innerHTML= "남은 쉬는시간은...<br>" + minute + "분 " + second+" 초";
+    if(stopping){
+        stopping=false;
+        text.innerHTML = "중지되었습니다.";
+        return;
+    }
+    else if(diff > 0){
         setTimeout(resting, 100, started);
     }
     else{
         showTime(now);
     }
+}
+
+function stop(){    
+    stopping = true;
+    start=false;
 }
